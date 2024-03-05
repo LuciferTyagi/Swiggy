b072cc35da4612de39d82ff730e055c72867ac45
Yagyansh Tyagi <78792172+LuciferTyagi@users.noreply.github.com>
2024-03-05 10:37:32 +0530

    classComponents

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,14 @@
+import  User from "./User";
+import UserClass from "./UserClass";
 const About = ()=> {
 return (
     <div>
         <h1>About-Us</h1>
         <h2>This is my About us page</h2>
+        <User name={"Lucifer Tyagi"}
+              location = {"Meerut"}  
+        />
+        <UserClass />
     </div>
 )
 }
---
+++ src/Components/UserClass.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class UserClass extends React.Component{
+
+    render() {
+        
+        return (
+            <div className="user-card">
+                <h2>Name : Yagyansh</h2>
+                <h2>Location : Hapur</h2>
+            </div>
+        )
+        
+    }
+}
+
+export default UserClass;
