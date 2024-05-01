bf62c0494cfcec5070a196688e471235f97f68bd
Yagyansh Tyagi <78792172+LuciferTyagi@users.noreply.github.com>
2024-05-01 12:03:28 +0530

    Local Storage

--- src/utlis/cartSlice.js
+++ src/utlis/cartSlice.js
@@ -1,29 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    items: [],
+    items: localStorage.getItem("items")
+      ? JSON.parse(localStorage.getItem("items"))
+      : [],
     status: null,
   },
   reducers: {
-    addItem: (state, action ) => {
+    addItem: (state, action) => {
       const itemIndex = state.items.findIndex(
         (item) => item.card.info.id === action.payload.card.info.id
       );
 
       if (itemIndex >= 0) {
         state.items[itemIndex].cartQuantity += 1;
-        toast.info("Increased product quantity",{
-          position:"bottom-left"
-        })
+        toast.info(
+          `Increased the quantity of ${action.payload.card.info.name}`,
+          {
+            position: "bottom-left",
+          }
+        );
       } else {
         const tempItem = { ...action.payload, cartQuantity: 1 };
         state.items.push(tempItem);
-        toast.success(`${action.payload.card.info.name}Added a new product to cart`,{
-          position:"bottom-left"
-        })
+        toast.success(`${action.payload.card.info.name} added to cart`, {
+          position: "bottom-left",
+        });
       }
+      localStorage.setItem("items", JSON.stringify(state.items));
     },
 
     removeItem: (state, action) => {
